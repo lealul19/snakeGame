12029e903869332d1c568eeceb1bcc794ac9339e
lealul19 <94169536+lealul19@users.noreply.github.com>
2025-06-05 10:47:41 +0200

    HelloApplication code

--- src/main/java/com/htlshkoder/sew_projekt_snakegame/HelloApplication.java
+++ src/main/java/com/htlshkoder/sew_projekt_snakegame/HelloApplication.java
@@ -1,20 +1,21 @@
 package com.htlshkoder.sew_projekt_snakegame;
 
+
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-import java.io.IOException;
-
 public class HelloApplication extends Application {
     @Override
-    public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+    public void start(Stage stage) throws Exception {
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("/com/htlshkoder/projekt_prov/hello-view.fxml"));
+        Scene scene = new Scene(fxmlLoader.load());
+        stage.setTitle("Snake Game FX");
         stage.setScene(scene);
         stage.show();
+        System.out.println("FXML path = " + getClass().getResource("/fxml/game-view.fxml"));
+
     }
 
     public static void main(String[] args) {
